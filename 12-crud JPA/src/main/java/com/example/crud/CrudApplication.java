837c377f8e17450b6038f75d2bbfeb3603348c8f
alizamemon <alizanisar11@gmail.com>
2026-06-10 17:59:03 +0500

    hibernate JPA

---
+++ 12-crud JPA/src/main/java/com/example/crud/CrudApplication.java
@@ -0,0 +1,22 @@
+package com.example.crud;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext context) {
+		return runner -> {
+			System.out.println("Hello World");
+		};
+	}
+}
